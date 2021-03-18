3a98bef9469a3bdbd0da9566a42b3b56b5cd2eb0
Andreas Velounias <>
2021-03-18 12:55:55 +0000

    Implement domain layer

--- app/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ app/lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -1,27 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:numbers_trivia/core/error/failures.dart';
 import 'package:numbers_trivia/core/usecases/usecases.dart';
 import 'package:numbers_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:numbers_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia implements UseCase<NumberTrivia, Params> {
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
 
-  GetConcreteNumberTrivia(this.repository);
-
+  GetRandomNumberTrivia(this.repository);
   @override
-  Future<Either<Failure, NumberTrivia>> call({Params params}) async {
-    return await repository.getConcreteNumberTrivia(params.number);
+  Future<Either<Failure, NumberTrivia>> call({NoParams params}) async {
+    return await repository.getRandomNumberTrivia();
   }
-}
-
-class Params extends Equatable {
-  final int number;
-
-  Params({@required this.number});
-
-  @override
-  List<Object> get props => [number];
 }
--- app/test/features/number_trivia/domain/usercases/get_random_number_trivia_test.dart
+++ app/test/features/number_trivia/domain/usercases/get_random_number_trivia_test.dart
@@ -1,38 +1,38 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:numbers_trivia/core/usecases/usecases.dart';
 import 'package:numbers_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:numbers_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:numbers_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:numbers_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock
     implements NumberTriviaRepository {}
 
 void main() {
-  GetConcreteNumberTrivia usecase;
+  GetRandomNumberTrivia usecase;
   MockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
 
-  final testNumber = 1;
-  final testNumberTrivia = NumberTrivia(text: 'test', number: testNumber);
+  final testNumberTrivia = NumberTrivia(text: 'test', number: 1);
 
   tearDown(() {});
 
   test('should get trivia for the number from the repository', () async {
     // Arrange
-    when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+    when(mockNumberTriviaRepository.getRandomNumberTrivia())
         .thenAnswer((_) async => Right(testNumberTrivia));
 
     // Act
-    final result = await usecase(params: Params(number: testNumber));
+    final result = await usecase(params: NoParams());
 
     // Assert
     expect(result, Right(testNumberTrivia));
-    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(testNumber));
+    verify(mockNumberTriviaRepository.getRandomNumberTrivia());
     verifyNoMoreInteractions(mockNumberTriviaRepository);
   });
 }
